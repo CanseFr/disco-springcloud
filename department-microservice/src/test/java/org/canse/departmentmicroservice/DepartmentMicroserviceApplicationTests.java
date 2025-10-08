e789cdb9a5a9f3ddf327023c4aefbbab79d1cd29
CanseFr <jucansell@gmail.com>
2025-10-08 21:13:54 +0200

    :sparkles: Create department microservice

---
+++ department-microservice/src/test/java/org/canse/departmentmicroservice/DepartmentMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.canse.departmentmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
