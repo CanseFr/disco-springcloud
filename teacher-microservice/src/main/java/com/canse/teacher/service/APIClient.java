8ea44dae7d0bfae0bba3c1d6f6e99a0ae91fb727
CanseFr <jucansell@gmail.com>
2025-10-09 22:47:51 +0200

    :recycle: Delete url because value service register for eureka

--- teacher-microservice/src/main/java/com/canse/teacher/service/APIClient.java
+++ teacher-microservice/src/main/java/com/canse/teacher/service/APIClient.java
@@ -5,7 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080" , value = "DEPARTMENT-MICROSERVICE")
+//Maintenant que nous utilison Eureka, il n'est plus necessaire de preciser une url car le nom du service est enregistré pour chaque instance du service
+//@FeignClient(url = "http://localhost:8080" , value = "DEPARTMENT-MICROSERVICE")
+@FeignClient(value = "DEPARTMENT-MICROSERVICE")
 public interface APIClient {
 
     @GetMapping("api/departments/{department-code}")
