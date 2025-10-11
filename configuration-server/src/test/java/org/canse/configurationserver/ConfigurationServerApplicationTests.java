aa89dff4243ac45b318678d5a01cf7cda875ed8c
CanseFr <jucansell@gmail.com>
2025-10-11 12:35:30 +0200

    :tada: Create config server micro service, with all service config file of project

---
+++ configuration-server/src/test/java/org/canse/configurationserver/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.canse.configurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
